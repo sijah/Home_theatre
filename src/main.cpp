76ff2b7897e45f8a27fa2dd51ae80288fcf4d2d4
sijah.ak <sijah.ak@gmail.com>
2020-08-16 21:13:43 +0530

    init

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+ESP8266WebServer server(80);
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
