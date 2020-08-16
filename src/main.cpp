c793f9c59d7cd7a372e46121285b59ab96f09ee4
sijah.ak <sijah.ak@gmail.com>
2020-08-16 21:17:04 +0530

    init

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 ESP8266WebServer server(80);
+const char *ssid ="";
+const char *pass="";
 
 void setup() {
   // put your setup code here, to run once:
