ee00b7f089d1fceb571c8383622288a13bcbc6fd
sijah.ak <sijah.ak@gmail.com>
2020-08-21 21:03:56 +0530

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <Wire.h>
+#include <TDA7439.h>
 ESP8266WebServer server(80);
+TDA7439 audio = TDA7439();
 const char *ssid ="";
 const char *pass="";
-
 void setup() {
-  // put your setup code here, to run once:
+  audio.setInput(1);
 }
 
 void loop() {
